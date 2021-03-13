be7aba13accaba9e2dea403fdc45a94007c10e03
Sajeel Hassan <hsajeel.dev@gmail.com>
2021-03-13 23:13:23 +0500

    :rocket:

--- src/api.js
+++ src/api.js
@@ -1,17 +1,18 @@
 class API {
     constructor() {
         this.key = "at_IlPSwRjGGETJEwUpoMUmRlU3pKctL";
-        this.ipAddress = "111.68.103.27";
+        this.sendingInfo = "39.37.231.125";
 
     }
     async getData() {
-        const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=${this.key}&ipAddress=${this.ipAddress}`);
-        const data = await response.json();
-
+        let apiUi = new UI();
+        apiUi.loadingUI();
+        const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=${this.key}&ipAddress=${this.sendingInfo}`);
+        var data = await response.json();
         return data;
 
     }
-    updateIP(ip) {
-        this.ipAddress = ip;
+    updateInfo(info) {
+        this.sendingInfo = info;
     }
 }
